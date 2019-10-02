e8a6764ffb4eeb26a8ab03e2a9d86b1ff843a998
Jeffrey Hutchins <jeffrey.w.hutchins@gmail.com>
2019-10-02 11:39:33 -0500

    In Progress

---
+++ probability_distributions.py
@@ -0,0 +1,47 @@
+# Do your work for this exercise in either a python script named probability_distributions.py 
+# or a jupyter notebook named probability_distributions.ipynb.
+
+# For the following problems, use python to simulate the problem and calculate an experimental 
+# ability, then compare that to the theoretical probability.
+
+# 1. A bank found that the average number of cars waiting during the noon hour at a drive-up 
+# window follows a Poisson distribution with a mean of 2 cars. Make a chart of this 
+# distribution and answer these questions concerning the probability of cars waiting 
+# at the drive-up window.
+
+# 2. Grades of State University graduates are normally distributed with a mean of 3.0 and a 
+# standard deviation of .3. Calculate the following:
+
+# What grade point average is required to be in the top 5% of the graduating class?
+
+# An eccentric alumnus left scholarship money for students in the third decile from the 
+# bottom of their class. Determine the range of the third decile. Would a student with a 
+# 2.8 grade point average qualify for this scholarship?
+
+
+
+# 3. A marketing website has an average click-through rate of 2%. One day they observe 4326 
+# visitors and 97 click-throughs. How likely is it that this many people or more click through?
+
+# 4. You are working on some statistics homework consisting of 100 questions where all of 
+# the answers are a probability rounded to the hundreths place. Looking to save time, 
+# you put down random probabilities as the answer to each question.
+
+# What is the probability that at least one of your first 60 answers is correct?
+
+
+
+# 5. The codeup staff tends to get upset when the student break area is not cleaned up. 
+# Suppose that there's a 3% chance that any one student cleans the break area when 
+# they visit it, and, on any given day, about 90% of the 3 active cohorts of 22 
+# students visit the break area. How likely is it that the break area gets cleaned 
+# up each day? How likely is it that it goes two days without getting cleaned up? All week?
+
+# 6. You want to get lunch at La Panaderia, but notice that the line is usually very 
+# long at lunchtime. After several weeks of careful observation, you notice that 
+# the average number of people in line when your lunch break starts is normally 
+# distributed with a mean of 15 and standard deviation of 3. If it takes 2 minutes 
+# for each person to order, and 10 minutes from ordering to getting your food, what 
+# is the likelihood that you have at least 15 minutes left to eat your food before 
+# you have to go back to class? Assume you have one hour for lunch, and ignore 
+# travel time to and from La Panaderia.
