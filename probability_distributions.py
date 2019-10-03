3bf03d6b7fa70a6149031cc2332fa530b5a02bd5
Jeffrey Hutchins <jeffrey.w.hutchins@gmail.com>
2019-10-03 11:14:56 -0500

    Completed

--- probability_distributions.py
+++ probability_distributions.py
@@ -82,11 +82,18 @@ pd.DataFrame(np.random.normal(3, .3, trials)).quantile(.95)
     # 2.8 grade point average qualify for this scholarship?
 
 # theoretical
-grades_dist.isf(.8)
-grades_dist.isf(.7)
+grades_dist.ppf(.7)
+grades_dist.ppf(.8)
+
+grades_dist.isf(.3)
+grades_dist.isf(.2)
 
 # experimental
+np.quantile(grades_array, .7)
+np.quantile(grades_array, .8)
 
+pd.DataFrame(np.random.normal(3, .3, trials)).quantile(.7)
+pd.DataFrame(np.random.normal(3, .3, trials)).quantile(.8)
 
 # 3. A marketing website has an average click-through rate of 2%. One day they observe 4326 
 # visitors and 97 click-throughs. How likely is it that this many people or more click through?
@@ -99,15 +106,14 @@ click_throughs = 97
 stats.poisson(.02*4326).sf(97) * 100
 
 # experimental
+(pd.DataFrame(np.random.poisson(4326*.02, 10000)) >= 97).mean()
+
 
 # 4. You are working on some statistics homework consisting of 100 questions where all of 
     # the answers are a probability rounded to the hundreths place. Looking to save time, 
     # you put down random probabilities as the answer to each question.
 
-# theoretical
-answers = np.round(np.random.sample(100), decimals=2)
-
-# experimental
+answers = pd.DataFrame(np.round(np.random.sample(100), decimals=2))
 
     # What is the probability that at least one of your first 60 answers is correct?
 
@@ -116,7 +122,8 @@ stats.binom(60, .01).sf(0)
 stats.binom(100, .01).sf(0)
 
 # experimental
-
+(np.random.binomial(60, .01, 10000) > 0).mean()
+(np.random.binomial(100, .01, 10000) > 0).mean()
 
 # 5. The codeup staff tends to get upset when the student break area is not cleaned up. 
 # Suppose that there's a 3% chance that any one student cleans the break area when 
@@ -127,12 +134,14 @@ stats.binom(100, .01).sf(0)
 # theoretical
 students = round(.9*(22*3))
 student_cleans = .03*students
-stats.binom(students, .03).sf(0) # each day
-stats.binom(students, .03).sf(1) # two days
-stats.binom(students, .03).sf(4) # 5 days
+stats.binom(students, .03).cdf(0) # each day
+ 1 - (stats.binom(students, .03).cdf(1)) # two days
+1 - (stats.binom(students, .03).cdf(4)) # 5 days
 
 # experimental
-
+(np.random.binomial(students, .03, 10000) == 0).mean()
+(np.random.binomial(students, .03, 10000) >= 2).mean()
+(np.random.binomial(students, .03, 10000) >= 5).mean()
 
 # 6. You want to get lunch at La Panaderia, but notice that the line is usually very 
 # long at lunchtime. After several weeks of careful observation, you notice that 
@@ -147,6 +156,67 @@ stats.binom(students, .03).sf(4) # 5 days
 stats.norm(40, 6).cdf(45)
 
 # experimental
+(np.random.normal(40, 6, 10000) < 45).mean()
+
+# 7. Connect to the employees database and find the average salary of current employees, 
+# along with the standard deviation. Model the distribution of employees salaries with 
+# a normal distribution and answer the following questions:
+
+from env import host, user, password
+database_name = "employees"
+url = f'mysql+pymysql://{user}:{password}@{host}/{database_name}'
+
+employees_query = "SELECT * from employees"
+employees = pd.read_sql(employees_query, url)
+employees.head()
+
+salaries_query = "SELECT * from salaries where to_date = '9999-01-01'"
+salaries = pd.read_sql(salaries_query, url)
+
+type(salaries)
+salaries_mean = salaries.salary.mean()
+salaries_std = salaries.salary.std()
+
+# theoretical
+stats.norm(salaries_mean, salaries_std)
 
 
+# experimental
+salaries_np = np.random.normal(salaries_mean, salaries_std, 10000)
+salaries_dataframe = pd.DataFrame(np.random.normal(salaries_mean, salaries_std, 10000))
+
+    # What percent of employees earn less than 60,000?
+
+# theoretical
+stats.norm(salaries_mean, salaries_std).cdf(60000)
+
+# experimental
+(salaries_dataframe < 60000).mean()
+
+    # What percent of employees earn more than 95,000?
+
+# theoretical
+stats.norm(salaries_mean, salaries_std).sf(95000)
+
+# experimental
+(salaries_dataframe > 95000).mean()
+
+    # What percent of employees earn between 65,000 and 80,000?
+
+# theoretical
+stats.norm(salaries_mean, salaries_std).cdf(80000) - stats.norm(salaries_mean, salaries_std).cdf(65000)
+
+
+# experimental
+salaries_np.where(np.logical_and(a>60000, a<80000))
+salaries_np
+
+    # What do the top 5% of employees make?
+
+# theoretical
+stats.norm(salaries_mean, salaries_std).ppf(.95)
+stats.norm(salaries_mean, salaries_std).ppf(.9999996825)
+
+# experimental
+salaries_dataframe.quantile(.95)
 
